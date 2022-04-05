5ae4b3a1867129638f56a675547dc62868131590
wallaceAzevedo <wallaceazevedo17@gmail.com>
2022-04-05 20:29:33 -0300

    next level

---
+++ src/pages/index.tsx
@@ -0,0 +1,33 @@
+import Layout from '../components/Layout'
+import Tabela from '../components/Tabela'
+import Cliente from '../core/Cliente'
+
+export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 45, '2'),
+    new Cliente('Carlos', 23, '3'),
+    new Cliente('Pedro', 54, '4'),
+  ]
+
+  function clienteSeleciocado(cliente: Cliente){
+
+  }
+  function clienteExcluido(cliente: Cliente){
+
+  }
+  
+  return (
+    <div className={`
+      flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white
+    `}>
+      <Layout titulo="Cadastro Simples" >
+        <Tabela clientes={clientes} 
+        clienteSelecionado={clienteSeleciocado} 
+        clienteExcluido={clienteExcluido}
+        ></Tabela>
+      </Layout>
+    </div>
+  )
+}
